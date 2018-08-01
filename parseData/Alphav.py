7934941204270c4b102cfcb7db1de6febc6d166b
xinging_birds <xinglongjh@gmail.com>
2018-08-01 08:43:06 -0400

    Add more project management files.

--- parseData/Alphav.py
+++ parseData/Alphav.py
@@ -7,8 +7,9 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 import matplotlib.pyplot as plt
 
-# used for printing out pandas dataframes
-from pprint import pprint
+
+# used for printing out pandas data frames
+# from pprint import pprint
 
 class Alphav(object):
     def __init__(self, ticker, years):
@@ -16,22 +17,23 @@ class Alphav(object):
         self.years = years
 
     def daily_data(self):
-        ts = TimeSeries(key = "Q63YHW130JGQ7JPT", output_format = "pandas", indexing_type = "date")
-        data, meta_data = ts.get_daily_adjusted(symbol = self.ticker, outputsize = "full")
-        start = (datetime.strptime(data.index.values[-1], "%Y-%m-%d") - relativedelta(years=self.years)).strftime("%Y-%m-%d")
+        ts = TimeSeries(key="Q63YHW130JGQ7JPT", output_format="pandas", indexing_type="date")
+        data, meta_data = ts.get_daily_adjusted(symbol=self.ticker, outputsize="full")
+        start = (datetime.strptime(data.index.values[-1], "%Y-%m-%d") - relativedelta(years=self.years)).strftime(
+            "%Y-%m-%d")
 
         target = data.loc[start:data.index.values[-1], ["5. adjusted close", "2. high", "3. low", "6. volume"]]
         target.columns = ["adj_close", "high", "low", "volume"]
-        return(target)
+        return (target)
 
     def indics(self, output):
-        ti = TechIndicators(key = "Q63YHW130JGQ7JPT", output_format = "pandas", indexing_type = "date")
+        ti = TechIndicators(key="Q63YHW130JGQ7JPT", output_format="pandas", indexing_type="date")
         getindic = getattr(self, output, lambda: "Invalid indicator.")
 
         return getindic(ti)
 
     def bbands(self, ti):
-        data, meta_data = ti.get_bbands(symbol = self.ticker)
+        data, meta_data = ti.get_bbands(symbol=self.ticker)
         parse = getattr(self, "process", lambda: "An error occurred.")
         return parse(data)
 
@@ -46,12 +48,12 @@ class Alphav(object):
         return parse(data)
 
     def rsi(self, ti):
-        data, meta_data = ti.get_rsi(symbol=self.ticker, time_period = 14)
+        data, meta_data = ti.get_rsi(symbol=self.ticker, time_period=14)
         parse = getattr(self, "process", lambda: "An error occurred.")
         return parse(data)
 
     def chaikin(self, ti):
-        data, meta_data = ti.get_ad(symbol = self.ticker)
+        data, meta_data = ti.get_ad(symbol=self.ticker)
         parse = getattr(self, "process", lambda: "An error occurred.")
         return parse(data)
 
@@ -61,30 +63,31 @@ class Alphav(object):
         return parse(data)
 
     def aroon(self, ti):
-        data, meta_data = ti.get_aroon(symbol = self.ticker, time_period = 14)
+        data, meta_data = ti.get_aroon(symbol=self.ticker, time_period=14)
         parse = getattr(self, "process", lambda: "An error occurred.")
         return parse(data)
 
     def process(self, data):
-        start = (datetime.strptime(data.index.values[-1], "%Y-%m-%d %H:%M:%S") - relativedelta(years=self.years)).strftime("%Y-%m-%d")
+        start = (datetime.strptime(data.index.values[-1], "%Y-%m-%d %H:%M:%S") - relativedelta(
+            years=self.years)).strftime("%Y-%m-%d")
         target = data.loc[start:data.index.values[-1]]
-        return(target)
+        return target
 
     def assemble(self):
         indics = ["bbands", "ema", "stoch", "rsi", "chaikin", "obv", "aroon"]
         prices = self.daily_data()
-        master = prices.reset_index(drop = True)
+        master = prices.reset_index(drop=True)
         print("Successfully parsed stock data.")
 
-        for i in range(0,len(indics)):
-            if (i == 3):
+        for i in range(0, len(indics)):
+            if i == 3:
                 print("Waiting for 60 seconds")
                 time.sleep(60)
-            master = pd.concat([master, self.indics(indics[i]).reset_index(drop = True)], axis = 1, sort = False)
-            print("Successfully parsed %s."%(indics[i]))
+            master = pd.concat([master, self.indics(indics[i]).reset_index(drop=True)], axis=1, sort=False)
+            print("Successfully parsed %s." % (indics[i]))
 
-        master.set_index(prices.index.values, inplace = True)
+        master.set_index(prices.index.values, inplace=True)
         master.plot()
-        plt.title("Stock Prices and Indicators for %s"%(self.ticker))
+        plt.title("Stock Prices and Indicators for %s" % self.ticker)
         plt.show()
         return master
--- parseData/Datagen.py
+++ parseData/Datagen.py
@@ -3,4 +3,4 @@ from Alphav import Alphav
 ticker = input("Enter the ticker: ")
 comp = Alphav(ticker, 10)
 # time.sleep(40)
-comp.assemble().to_csv("alpha_%s.csv"%(ticker), encoding = "utf-8")
+comp.assemble().to_csv("alpha_%s.csv"%ticker, encoding="utf-8")
