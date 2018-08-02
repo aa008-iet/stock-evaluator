cffd1a429069255f279ba11c614fa061cc0b50aa
xinging_birds <xinglongjh@gmail.com>
2018-08-02 13:57:44 -0400

    Add README

--- parseData/Morningstar.py
+++ parseData/Morningstar.py
@@ -8,7 +8,7 @@ class Morningstar(object):
     def readratios(self):
         url = "http://financials.morningstar.com/ajax/exportKR2CSV.html?t=%s" % self.ticker
         print("Now reading %s" % url)
-        response = pd.read_csv(url, skiprows=2).transpose()
+        response = pd.read_csv(url, skiprows=2).transpose().dropna(axis=1)
         response.columns = response.iloc[0]
         return response[1:]
 
