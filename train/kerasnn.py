da685f7de70470e11fea3b8dd94c1a2d4c91400d
xinging_birds <xinglongjh@gmail.com>
2018-08-03 16:30:46 -0400

    Update Keras base classifier.

--- train/kerasnn.py
+++ train/kerasnn.py
@@ -7,23 +7,23 @@ from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import cross_val_score
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import StratifiedKFold
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import Pipeline
+# from sklearn.preprocessing import StandardScaler
+# from sklearn.pipeline import Pipeline
 
 # "set seed for reproducibility"
 seed = 7
 numpy.random.seed(7)
 # load test data set
-csv = pd.read_csv("test.csv", index_col="Unnamed: 0")
+csv = pd.read_csv("test.csv", index_col="Unnamed: 0", thousands=',')
 set = csv.values
 # define input and output variables
-x = set[:, 0:len(csv.columns)].astype(float)
-y = set[:, len(csv.columns)]
+x = set[:, 0:(len(csv.columns) - 1)].astype(float)
+y = set[:, (len(csv.columns) - 1)]
 # encode if not set to 0s and 1s already (but they will be)
 encoded_Y = LabelEncoder().fit(y).transform(y)
 
 
-# baseline model
+# baseline model - evaluates w/ 49% accuracy
 def based_model():
     m = Sequential()
     m.add(Dense(72, input_dim=72, kernel_initializer='normal', activation='relu'))
